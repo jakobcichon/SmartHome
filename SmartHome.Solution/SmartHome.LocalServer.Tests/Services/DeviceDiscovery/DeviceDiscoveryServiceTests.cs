e6cb503e2fd3a955a26158561b35a52fc8a48059
CichonJ <jakob.cichon@aebi-schmidt.com>
2025-05-20 10:34:40 +0200

    Added test case. Tests are passing

--- SmartHome.Solution/SmartHome.LocalServer.Tests/Services/DeviceDiscovery/DeviceDiscoveryServiceTests.cs
+++ SmartHome.Solution/SmartHome.LocalServer.Tests/Services/DeviceDiscovery/DeviceDiscoveryServiceTests.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using Microsoft.Extensions.Options;
 using Moq;
 using SmartHome.Common.Extensions.String;
@@ -9,30 +8,64 @@ namespace SmartHome.LocalServer.Tests.Services.DeviceDiscovery;
 
 public class DeviceDiscoveryServiceTests
 {
-    [Fact]
-    public void ExecuteAsync_DummyDeviceRequest_CallsTheRespondMethod()
+    IOptions<SmartHomeSettingsModel> _options;
+    public DeviceDiscoveryServiceTests()
     {
-        //Arrange
-        var options = Options.Create(
+        _options = Options.Create(
             new SmartHomeSettingsModel()
             {
                 LocalDeviceCall = "dummyLocalCall",
-                ServerCallResponse = "serverResponse"
+                ServerCallResponse = "dummyServerResponse"
             });
+    }
+
+    [Fact]
+    public void ExecuteAsync_ValidDeviceRequest_CallsTheRespondMethod()
+    {
+        //Arrange
         var mock = new Mock<IDeviceDiscoveryCommunicationInterface>();
-        mock.Setup(e => e.ReceiveDataAsync(CancellationToken.None)).
-            Returns(Task.FromResult(options.Value.LocalDeviceCall.ToUtf8()));
-        
-        var dut = new DeviceDiscoveryService(mock.Object, options);
-       
+        var stoppingtokenSource = new CancellationTokenSource();
+
+        mock.Setup(e => e.ReceiveDataAsync(It.IsAny<CancellationToken>()))
+            .Callback((CancellationToken token) =>
+            {
+                stoppingtokenSource.Cancel();
+            }).ReturnsAsync(_options.Value.LocalDeviceCall.ToUtf8());
+
+        var act = new DeviceDiscoveryService(mock.Object, _options);
+
         //Act
-        dut.StartAsync(CancellationToken.None);
+        act.StartAsync(stoppingtokenSource.Token);
 
         //Assert
         mock.Verify(e => e.ReceiveDataAsync(
-            It.IsAny<CancellationToken>()), Times.AtLeastOnce());
+            It.IsAny<CancellationToken>()), Times.Once());
         mock.Verify(e => e.SendDataAsync(
-            options.Value.ServerCallResponse.ToUtf8(), It.IsAny<CancellationToken>()));
+            _options.Value.ServerCallResponse.ToUtf8(), It.IsAny<CancellationToken>()));
+    }
+
+    [Fact]
+    public void ExecuteAsync_InvalidDeviceRequest_DontCallTheRespondMethod()
+    {
+        //Arrange
+        var mock = new Mock<IDeviceDiscoveryCommunicationInterface>();
+        var stoppingtokenSource = new CancellationTokenSource();
+
+        mock.Setup(e => e.ReceiveDataAsync(It.IsAny<CancellationToken>()))
+            .Callback((CancellationToken token) =>
+            {
+                stoppingtokenSource.Cancel();
+            }).ReturnsAsync("incorrectDeviceCall".ToUtf8());
+
+        var act = new DeviceDiscoveryService(mock.Object, _options);
+
+        //Act
+        act.StartAsync(stoppingtokenSource.Token);
+
+        //Assert
+        mock.Verify(e => e.ReceiveDataAsync(
+            It.IsAny<CancellationToken>()), Times.Once());
+        mock.Verify(e => e.SendDataAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()), Times.Never());
 
     }
 }
