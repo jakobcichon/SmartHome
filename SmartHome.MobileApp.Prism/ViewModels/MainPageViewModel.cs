d12cbbf855b11daa62b8f96cc9a868978da84ffc
CichonJ <jakob.cichon@aebi-schmidt.com>
2025-07-04 07:25:14 +0200

    Added menu navigation commands

--- SmartHome.MobileApp.Prism/ViewModels/MainPageViewModel.cs
+++ SmartHome.MobileApp.Prism/ViewModels/MainPageViewModel.cs
@@ -4,22 +4,33 @@ namespace SmartHome.MobileApp.Prism.ViewModels
     internal class MainPageViewModel : BindableBase, INavigationAware
     {
         public string MainPageRegionName { get; } = "MainPageContent";
+        public DelegateCommand<INavigationParameters> NavigateToHomeCommand { get; private set; }
+        public DelegateCommand<INavigationParameters> NavigateToMenuCommand { get; private set; }
 
         private readonly IRegionManager _regionManager;
 
         public MainPageViewModel(IRegionManager regionManager)
         {
             _regionManager = regionManager;
+            NavigateToHomeCommand = new(OnNavigatedTo);
+            NavigateToMenuCommand = new(OnNavigateToMenu);
         }
 
         public void OnNavigatedFrom(INavigationParameters parameters)
         {
-            ;
+          
         }
 
         public void OnNavigatedTo(INavigationParameters parameters)
         {
             _regionManager.RequestNavigate(MainPageRegionName, "HomeView");
         }
+
+        public void OnNavigateToMenu(INavigationParameters parameters)
+        {
+            _regionManager.RequestNavigate(MainPageRegionName, "MenuView");
+        }
+
+
     }
 }
