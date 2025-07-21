6723da87c38401c494eb1a3ee1025ed7277929ca
jakobcichon <58509316+jakobcichon@users.noreply.github.com>
2025-07-21 06:47:06 +0200

    Add serve discovery service

--- SmartHome.MobileApp.Prism/MauiProgram.cs
+++ SmartHome.MobileApp.Prism/MauiProgram.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Example;
+using Microsoft.Extensions.Logging;
+using SmartHome.Common.Extensions;
 using SmartHome.MobileApp.Prism.ViewModels;
 using SmartHome.MobileApp.Prism.ViewModels.Devices;
 using SmartHome.MobileApp.Prism.ViewModels.Menu;
 using SmartHome.MobileApp.Prism.Views;
 using SmartHome.MobileApp.Prism.Views.Devices;
 using SmartHome.MobileApp.Prism.Views.Menu;
+using SmartHome.MobileApp.Services.LocalServerServices;
 
 namespace SmartHome.MobileApp.Prism
 {
@@ -29,13 +32,14 @@ namespace SmartHome.MobileApp.Prism
                     container.RegisterForNavigation<DevicesListView, DevicesListViewModel>();
                     container.RegisterForNavigation<MenuView, MenuViewModel>();
 
-                    container.Register<>
+                    container.Register<IServerDiscoveryService, ServerDiscoveryService>();  
 
 
                 })
                 .CreateWindow("NavigationPage/MainPage");
 
             });
+            builder.Configuration.AddCommonConfiguration();
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
